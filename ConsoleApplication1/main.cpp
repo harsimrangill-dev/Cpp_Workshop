0c5ecf60dba4684f7d10cd8f444db4bf128129ca
Your Name <you@example.com>
2023-10-05 14:34:06 +0100

    improvement on workshop-2 =>V.2

--- ConsoleApplication1/main.cpp
+++ ConsoleApplication1/main.cpp
@@ -21,42 +21,55 @@ int main() {
 	string word = "";
 	int numberOfSentences = 0;
 	int numberOfWords = 0;
+	string fileName = "text.txt";
 	vector<pairObj> pairs;
 
 	ifstream myFile;
-	myFile.open("text.txt");
+	myFile.open(fileName);
 	
 	if (myFile.is_open()) {
 		while (getline(myFile, sen, '.')) {
-			numberOfSentences++;					
-			stringstream  inter(sen);
-			string temp = "";
-			while (getline(inter, temp, ',')) {
-				int pos = -1;
-				pos = temp.find("\n");
+			numberOfSentences++;	
+				
+			int one = -1, two = -1, pos = -1;
+			one = sen.find(",");
+			while (one != -1) {				
+				sen = sen.substr(0, one) + " " + sen.substr(one + 1);
+				one = sen.find(",");
+			}
+			two = sen.find("!");
+			while (two != -1) {
+				sen = sen.substr(0, two) + sen.substr(two + 1);
+				two = sen.find("!");
+			}
+			pos = sen.find("\n");
+			while (pos != -1) {
 				if (pos == 0) {
-					temp = temp.substr(pos + 1);
+					sen =  sen.substr(pos + 1);
 				}
-				stringstream newtemp(temp);
-				
-				while (getline(newtemp, word, ' ')) {
-					int pary = 0;
-					numberOfWords++;
-					for (auto& x : pairs) {
-						if (x.word == word) {
-							x.count++;
-							pary++;
-							break;
-						}
-					}
-					if (pary == 0) {
-						pairObj mid;
-						mid.word = word; mid.count = 1;
-						pairs.push_back(mid);
-						
-					}
+				else {
+					sen = sen.substr(0, pos) + sen.substr(pos + 1);
 				}
 				
+				pos = sen.find("\n");
+			}
+			stringstream  inter(sen);
+
+			while (getline(inter, word, ' ')) {				
+				int pary = 0;
+				numberOfWords++;
+				for (auto& x : pairs) {
+					if (x.word == word) {
+						x.count++;
+						pary++;
+						break;
+					}
+				}
+				if (pary == 0) {
+					pairObj mid;
+					mid.word = word; mid.count = 1;
+					pairs.push_back(mid);
+				}											
 			}
 		}
 		myFile.close();
